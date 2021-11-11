66773ea738c92b28fca1fdce7375ddbf9437d81b
Charlie Highum <chighum41@gmail.com>
2021-11-10 21:33:29 -0700

    added functionality to answer buttons to say if the user is correct or incorrect. having difficulties with iteration over questions array as it is currently written when the user selects an answer to the 2nd question it skips to the 4th question. once I fix that bug I need to add the score saving aspect that saves to local storage and is displayed on the page with name as the key and score as the value

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,57 @@ var timerElement = document.getElementById("timer");
 var questionElement = document.getElementById("question");
 // controls the question element created in HTML
 var answerElement = document.getElementById("answers");
+var answerAElement = document.getElementById("answerA");
+var answerBElement = document.getElementById("answerB");
+var answerCElement = document.getElementById("answerC");
+var answerDElement = document.getElementById("answerD");
 answerElement.style.display = "none";
+var feedbackElement = document.getElementById("correctIncorrect");
 // format the answer options into an ordered list
 var yesPlay = document.querySelector("#yesButton");
 var noPlay = document.querySelector("#noButton");
 var startGame = document.querySelector("#start-game");
+var gameElement = document.querySelector("#game");
+
+var questions = [
+  (question1 = {
+    question: "Commonly used data types do NOT include:",
+    answer1: "strings",
+    correct: "alerts", // correct
+    answer3: "Booleans",
+    answer4: "numbers",
+  }),
+  (question2 = {
+    question: "The condition in an if / else statement is enclosed in:",
+    answer1: "quotes",
+    answer2: "curly brackets",
+    correct: "parenthesis", // correct
+    answer4: "Square brackets",
+  }),
+  (question3 = {
+    question: "Arrays in Javascript can be used to store:",
+    answer1: "numbers and strings",
+    answer2: "other arrays",
+    answer3: "Booleans",
+    correct: "All of the above", // correct
+  }),
+  (question4 = {
+    question:
+      "String values must be enclosed in _______ when being assigned to variables",
+    answer1: "commas",
+    answer2: "curly brackets",
+    correct: "quotes", // correct
+    answer4: "parentheses",
+  }),
+  (question5 = {
+    question:
+      "A very useful tool for development and debugging code by printing content to the debugger:",
+    answer1: "JavaScript",
+    answer2: "terminal/bash",
+    answer3: "for loops",
+    correct: "console.log", // correct
+  }),
+];
 
 // give the user a button to click in order to start the game
 // which in turn starts the timer and questions
@@ -18,10 +64,10 @@ yesPlay.addEventListener("click", function (event) {
   startGame.remove();
   startTimer();
 });
-
 // time also needs to be deducted when the user gets a question wrong
+
+var timeLeft = 60;
 function startTimer() {
-  var timeLeft = 60;
   startQuestions();
   answerElement.style.display = "";
 
@@ -35,64 +81,49 @@ function startTimer() {
     } else if (timeLeft === 1) {
       timerElement.textContent = "Hurry! Only " + timeLeft + " second left!";
       timeLeft--;
-    } // end the game and present option to input name for stat keeping
-    // local storage of name (key) and score (value)
-    // getItem - retreive key/value pair from local storage
-    // setItem - change key/value pair in local storage
-    else {
+    } else {
       clearInterval(timeInterval);
       timerElement.textContent = "";
+      gameElement.remove();
+      inputName();
     }
   }, 1000);
-  function startQuestions() {
-    questionElement.textContent = question1.question;
-    answerElement.children[0].children[0].textContent = question1.answers[0];
-    answerElement.children[1].children[0].textContent = question1.answers[1];
-    answerElement.children[2].children[0].textContent = question1.answers[2];
-    answerElement.children[3].children[0].textContent = question1.answers[3];
-  }
 }
+var i = 0;
+function startQuestions() {
+  questionElement.textContent = questions[i].question;
+  answerAElement.textContent = Object.values(questions[i])[1];
+  answerBElement.textContent = Object.values(questions[i])[2];
+  answerCElement.textContent = Object.values(questions[i])[3];
+  answerDElement.textContent = Object.values(questions[i])[4];
+  answerElement.addEventListener("click", function (event) {
+    var userClick = event.target.textContent; // store text content of user click
+    console.log(userClick);
+    if (userClick === questions[i].correct) {
+      // check if user was correct
+      feedbackElement.textContent = "Correct!";
+      i++;
+      startQuestions();
+    } else if (i < 5) {
+      // otherwise user was incorrect and run the following to reduce time
+      feedbackElement.textContent = "Incorrect!";
+      timeLeft = timeLeft - 10;
+      i++;
+      startQuestions();
+    } else {
+      inputName();
+    }
+  });
 
-// Questions
-var question1 = {
-  question: "Commonly used data types do NOT include:",
-  answers: ["strings", "alerts", "Booleans", "numbers"],
-  //   answer1: "strings",
-  //   answer2: "alerts", // correct
-  //   answer3: "Booleans",
-  //   answer4: "numbers",
-};
-console.log(question1.answers[0]);
-var question2 = {
-  question: "The condition in an if / else statement is enclosed in:",
-  answer1: "quotes",
-  answer2: "curly brackets",
-  answer3: "parenthesis", // correct
-  answer4: "Square brackets",
-};
+  // declare variable = value of user click
+  //   var userClick =
+  //   // if user clicked correct (questions[i].correct) then run first if
+  //   if () {
 
-var question3 = {
-  question: "Arrays in Javascript can be used to store:",
-  answer1: "numbers and strings",
-  answer2: "other arrays",
-  answer3: "Booleans",
-  answer4: "All of the above", // correct
-};
-
-var question4 = {
-  question:
-    "String values must be enclosed in _______ when being assigned to variables",
-  answer1: "commas",
-  answer2: "curly brackets",
-  answer3: "quotes", // correct
-  answer4: "parentheses",
-};
-
-var questionn5 = {
-  question:
-    "A very useful tool for development and debugging code by printing content to the debugger:",
-  answer1: "JavaScript",
-  answer2: "terminal/bash",
-  answer3: "for loops",
-  answer4: "console.log", // correct
-};
+  //   function inputName() {
+  //     // end the game and present option to input name for stat keeping
+  //     // local storage of name (key) and score (value)
+  //     // getItem - retreive key/value pair from local storage
+  //     // setItem - change key/value pair in local storage
+  //   }
+}
