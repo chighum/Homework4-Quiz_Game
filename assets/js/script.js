ba54249855f9dd69f6cbfc41f4c6d0d98d872496
Charlie Highum <chighum41@gmail.com>
2021-11-11 18:50:56 -0700

    rendered names and scores to leaderboard page - took a while to debug and kept adding scores that weren't in storage but finally added highScoreList.textContent = ''

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
 // VARIABLE DECLARATION
-var yesPlay = document.querySelector("#startButton");
-var leaderBoard = document.querySelector("#scoresButton");
-var startGame = document.querySelector("#start-game");
-var gameElement = document.querySelector("#game");
+// 1st page - option to play or show leaderboard
+var startGame = document.getElementById("start-game");
+var yesPlay = document.getElementById("start-button");
+var leaderBoard = document.getElementById("scores-button");
+
+// 2nd page - multiple choice quiz game
+var gameElement = document.getElementById("game");
 var timerElement = document.getElementById("timer");
+var timeInterval;
 var questionElement = document.getElementById("question");
 var answerElement = document.getElementById("answers");
 var answerAElement = document.getElementById("answerA");
@@ -12,18 +16,33 @@ var answerCElement = document.getElementById("answerC");
 var answerDElement = document.getElementById("answerD");
 // make the answer element hidden when the page is first loaded
 answerElement.style.display = "none";
-var feedbackElement = document.getElementById("correctIncorrect");
+var feedbackElement = document.getElementById("correct-incorrect");
+
+// 3rd page - show score and option to input name
+var nameScoreElement = document.getElementById("name-score");
 var displayScore = document.getElementById("score");
 var userName = document.getElementById("name");
 userName.style.display = "none";
-
 var submitNameButton = document.getElementById("submit-name");
 
+// 4th page - leaderboard
+var highScores = document.getElementById("high-scores");
+highScores.style.display = "none";
+var highScoreList = document.getElementById("score-list");
+var homeButton = document.getElementById("home-button");
+
+// get score history from local storage and store as an array in scoreHistory
+var scoreHistory = JSON.parse(localStorage.getItem("high-scores")) || [];
+
+// question set declared as objects inside an array
+// can call question string value using questions[i].question
+// can call answers string value using Object.values(questions[i][#]); # = 1-4
+// can check for correct using questions[i].correct
 var questions = [
   (question1 = {
     question: "Commonly used data types do NOT include:",
-    answers: "strings",
-    correct: "alerts", // correct
+    answer1: "strings",
+    correct: "alerts",
     answer3: "Booleans",
     answer4: "numbers",
   }),
@@ -31,7 +50,7 @@ var questions = [
     question: "The condition in an if / else statement is enclosed in:",
     answer1: "quotes",
     answer2: "curly brackets",
-    correct: "parenthesis", // correct
+    correct: "parenthesis",
     answer4: "Square brackets",
   }),
   (question3 = {
@@ -39,14 +58,14 @@ var questions = [
     answer1: "numbers and strings",
     answer2: "other arrays",
     answer3: "Booleans",
-    correct: "All of the above", // correct
+    correct: "All of the above",
   }),
   (question4 = {
     question:
       "String values must be enclosed in _______ when being assigned to variables",
-    answer1: "commas",
-    answer2: "curly brackets",
-    correct: "quotes", // correct
+    correct: "quotes",
+    answer2: "commas",
+    answer3: "curly brackets",
     answer4: "parentheses",
   }),
   (question5 = {
@@ -55,45 +74,63 @@ var questions = [
     answer1: "JavaScript",
     answer2: "terminal/bash",
     answer3: "for loops",
-    correct: "console.log", // correct
+    correct: "console.log",
   }),
 ];
+
+// Variables for iterating over time and questions
+var i;
+var score;
+var timeLeft;
+var gameOver = false;
 
 // give the user a button to click in order to start the game
 // which in turn starts the timer and questions
 // clear question and yes/no buttons from screen if they press yes
-yesPlay.addEventListener("click", function (event) {
-  event.preventDefault();
-  startGame.remove();
+yesPlay.addEventListener("click", function () {
+  startGame.style.display = "none";
+  gameOver = false;
+  i = 0;
+  score = 0;
+  timeLeft = 60;
   startTimer();
   startQuestions();
 });
 
 // button on home screen to show high scores
-leaderBoard.addEventListener("click", showLeaderBoard);
+leaderBoard.addEventListener("click", function () {
+  renderScores();
+  showLeaderBoard();
+});
 
 // 60 second timer that has different messages depending on how much time is left
-var timeLeft = 60;
 function startTimer() {
-  var timeInterval = setInterval(function () {
+  gameElement.style.display = "";
+  timeInterval = setInterval(function () {
+    timeLeft--;
     if (timeLeft > 10) {
       timerElement.textContent = timeLeft + " seconds left";
-      timeLeft--;
     } else if (timeLeft > 1) {
       timerElement.textContent = "Hurry! Only " + timeLeft + " seconds left!";
-      timeLeft--;
     } else if (timeLeft === 1) {
       timerElement.textContent = "Hurry! Only " + timeLeft + " second left!";
-      timeLeft--;
-    } else {
+    }
+    if (timeLeft >= 0) {
+      // if the questions run out and time is left clear the time interval and show input for stat keeping
+      if (gameOver && timeLeft > 0) {
+        clearInterval(timeInterval);
+        gameElement.style.display = "none";
+        inputName();
+      }
+    }
+    // end the game when the timer runs out and give option to input name for stat keeping
+    if (timeLeft === 0) {
       clearInterval(timeInterval);
-      gameElement.remove();
+      gameElement.style.display = "none";
       inputName();
     }
   }, 1000);
 }
-var i = 0;
-var score = 0;
 
 function startQuestions() {
   // refers to the value that corresponds to the question property of the ith question
@@ -108,28 +145,30 @@ function startQuestions() {
   // add one even listener to all the answer buttons and run function called checkCorrect
   answerElement.addEventListener("click", checkCorrect);
 }
+
 function checkCorrect(event) {
   // took forever to figure this out --
   // have to remove the event listener otherwise each iteration the click event fires multiple times
   answerElement.removeEventListener("click", checkCorrect);
   // store text content of user click
   var userClick = event.target.textContent;
-  // check if user was correct
+  // check if user was correct and add points to score
   if (userClick === questions[i].correct) {
     score = score + 50;
     feedbackElement.textContent = "Correct!";
-  } else if (userClick !== questions[i].correct) {
-    // otherwise user was incorrect and run the following to reduce time
+  } else {
+    // otherwise user was incorrect and reduce time
     feedbackElement.textContent = "Incorrect!";
-    timeLeft = timeLeft - 20;
+    timeLeft = timeLeft - 10;
   }
   i++;
-  // end the game when the questions run out
-  if (i === questions.length) {
-    gameElement.remove();
-    inputName();
-  } else {
+  // if there are still questions remaining, loop back to startQuestions function with the next index #
+  if (i < questions.length) {
     startQuestions();
+  } // end the game when the questions run out and set gameOver = true to end the game in the startTimer function
+  else {
+    gameOver = true;
+    gameElement.style.display = "none";
   }
 }
 
@@ -137,22 +176,44 @@ function inputName() {
   // end the game and present option to input name for stat keeping
   displayScore.textContent = "Score: " + score;
   // form for user to input name
+  nameScoreElement.style.display = "";
   userName.style.display = "";
-  submitNameButton.addEventListener("click", function (event) {
-    event.preventDefault();
-    var userNameInput = document.querySelector("#name-input").value;
-    localStorage.setItem("name", userNameInput);
-    localStorage.setItem("score", score);
-    showLeaderBoard();
-  });
+  submitNameButton.addEventListener("click", saveScores);
+  // store value of user name input
+}
 
-  // local storage of name (key) and score (value)
-  // getItem - retreive key/value pair from local storage
-  // setItem - change key/value pair in local storage
+// add the most recent score to local storage as an object
+function saveScores() {
+  submitNameButton.removeEventListener("click", saveScores);
+  var userNameInput = document.querySelector("#name-input").value;
+  var userNameScore = { name: userNameInput, score: score };
+  scoreHistory.push(userNameScore);
+  localStorage.setItem("high-scores", JSON.stringify(scoreHistory));
+  renderScores();
+  showLeaderBoard();
+}
+
+function renderScores() {
+  highScoreList.textContent = "";
+  for (var x = 0; x < scoreHistory.length; x++) {
+    // var singleScoreHistory = scoreHistory[x];
+    var listItem = document.createElement("li");
+    listItem.setAttribute("data-index", x);
+    listItem.textContent =
+      scoreHistory[x].name + " --- " + scoreHistory[x].score;
+    highScoreList.appendChild(listItem);
+  }
 }
 
 function showLeaderBoard() {
-  var lastUserName = localStorage.getItem("name");
-  var lastScore = localStorage.getItem("score");
-  alert(lastUserName + " " + lastScore);
+  nameScoreElement.style.display = "none";
+  startGame.style.display = "none";
+  highScores.style.display = "";
+
+  // alert(lastUserName + " " + lastScore);
 }
+
+homeButton.addEventListener("click", function () {
+  startGame.style.display = "";
+  highScores.style.display = "none";
+});
